b28706dbf2e58facb9d7e982af46a52278755ffc
carmel100 <160293061+carmel100@users.noreply.github.com>
2025-09-12 10:17:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app =  express()
 
 
 app.use(cors({
-  origin: "https://portfolio-front-end-delta.vercel.app/", // ton domaine vercel
+  origin: "https://portfolio-front-end-delta.vercel.app", // ton domaine vercel
   credentials: true
 }));;
 
