39d843c80786027cbd2fd67ca3308ccfec877105
carmel100 <160293061+carmel100@users.noreply.github.com>
2025-09-12 09:50:58 +0000

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -3,10 +3,10 @@
 
                    require('dotenv').config()
 
- const connexion =   mongoose.connect(process.env.MONGO_URI,{
+ const connexion =   mongoose.connect(process.env.MONGO_URI /*,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
- } )
+ }*/ )
   .then(() => console.log('connexion réussie á la base de données'))
   .catch(() => console.log('connexion impossible á la base de données '))
 
